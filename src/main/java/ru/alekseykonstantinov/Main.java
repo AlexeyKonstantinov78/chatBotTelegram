e4f7bcabb25a96031b7d40c8ae25af587072aba7
Aleksey <74707207+AlexeyKonstantinov78@users.noreply.github.com>
2025-04-27 14:23:58 +0300

    git add Config

---
+++ src/main/java/ru/alekseykonstantinov/Main.java
@@ -0,0 +1,11 @@
+package ru.alekseykonstantinov;
+
+import ru.alekseykonstantinov.config.Config;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Config config = new Config();
+    }
+}
---
+++ src/main/java/ru/alekseykonstantinov/config/Config.java
@@ -0,0 +1,34 @@
+package ru.alekseykonstantinov.config;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
+@Getter
+public class Config {
+
+    private static String TELEGRAM_BOT_NAME; //TODO: добавь имя бота в кавычках
+    private static String TELEGRAM_BOT_TOKEN; //TODO: добавь токен бота в кавычках
+
+    public Config() {
+        Properties props = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(input); // Загружаем файл
+
+            // Получаем значения
+            TELEGRAM_BOT_NAME = props.getProperty("telegram.bot.name");
+            TELEGRAM_BOT_TOKEN = props.getProperty("telegram.bot.token");
+
+        } catch (IOException e) {
+
+            log.error("Ошибка загрузки config.properties: " + e.getMessage());
+        }
+
+        log.info(TELEGRAM_BOT_NAME);
+    }
+}
+
