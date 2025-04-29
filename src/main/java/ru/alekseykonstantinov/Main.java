6e793295fee325c8af0d25e1718fbce3cf616db7
AlexeyKonstantinov78 <alex_kav@mail.ru>
2025-04-29 15:51:11 +0300

    update config

--- src/main/java/ru/alekseykonstantinov/Main.java
+++ src/main/java/ru/alekseykonstantinov/Main.java
@@ -3,7 +3,13 @@ package ru.alekseykonstantinov;
 import ru.alekseykonstantinov.config.Config;
 
 public class Main {
+    private static Config config;
+
+    public Main(Config config) {
+        this.config = new Config();
+    }
+
     public static void main(String[] args) {
-        Config config = new Config();
+
     }
 }
--- src/main/java/ru/alekseykonstantinov/config/Config.java
+++ src/main/java/ru/alekseykonstantinov/config/Config.java
@@ -9,26 +9,25 @@ import java.util.Properties;
 //@Slf4j
 @Getter
 public class Config {
-  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Config.class);
-  private static String TELEGRAM_BOT_NAME; //TODO: добавь имя бота в кавычках
-  private static String TELEGRAM_BOT_TOKEN; //TODO: добавь токен бота в кавычках
+    private final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Config.class);
+    private String TELEGRAM_BOT_NAME; //TODO: добавь имя бота в кавычках
+    private String TELEGRAM_BOT_TOKEN; //TODO: добавь токен бота в кавычках
 
-  public Config() {
-    Properties props = new Properties();
-    try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-      props.load(input); // Загружаем файл
+    public Config() {
+        Properties props = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(input); // Загружаем файл
 
-      // Получаем значения
-      TELEGRAM_BOT_NAME = props.getProperty("telegram.bot.name");
-      TELEGRAM_BOT_TOKEN = props.getProperty("telegram.bot.token");
+            // Получаем значения
+            TELEGRAM_BOT_NAME = props.getProperty("telegram.bot.name");
+            TELEGRAM_BOT_TOKEN = props.getProperty("telegram.bot.token");
 
-    } catch (IOException e) {
+        } catch (IOException e) {
 
-      log.error("Ошибка загрузки config.properties: " + e.getMessage());
-    } catch (RuntimeException e) {
-      log.error("Ошибка загрузки config.properties: " + e.getMessage());
+            log.error("Ошибка загрузки config.properties: " + e.getMessage());
+        } catch (RuntimeException e) {
+            log.error("Ошибка загрузки config.properties: " + e.getMessage());
+        }
     }
-    log.info("Проверка");
-  }
 }
 
